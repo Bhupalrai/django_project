3ae1d720a313711e4fa196447b9c873a62f62362
bhupalrai <nuraeen.rai@gmail.com>
2020-04-01 19:21:52 +0545

    Posts with ListView and Detail View

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from django.urls import path
 
+from .views import PostListView, PostDetailView
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='blog-home'),
+    #class based view needs certain naming convention
+    path('', PostListView.as_view(), name='blog-home'),
+    path('post/<int:pk>', PostDetailView.as_view(), name='post-detail'),
     path('about/', views.about, name='blog-about'),
 ]
